050ec0e873ca37b5d13258d6ca45916e5f721cf1
MGorin84 <gorinamaria@gmail.com>
2019-11-14 15:02:00 +1100

    commit

--- classes.rb
+++ classes.rb
@@ -1,13 +1,14 @@
 class PlushToys
-    attr_accessor :kind, :name, :colour, :accessories
+    attr_accessor :kind, :name, :colour, :accessories, :outfit
    
    def initialize(kind, name, colour, accessories)
     @kind = kind
     @name = name
     @colour = colour
+    @outfit = outfit
     @accessories = []
    end
-   
+
 end
 
   
--- main.rb
+++ main.rb
@@ -13,9 +13,29 @@ puts "Let's begin with choosing your fluffy friend?"
 choice_of_animal = prompt.select(" Please select one you like most!", %w(Bear Bunny Unicorn))
 kind = choice_of_animal.downcase
  
-choice_of_colour = prompt.select("What colour would you like your #{kind} to be?", %w( Red, Blue, Yellow, White, Black, Brown, Green, Pink, Rainbow)
-colour = choice_of_colour
+choice_of_colour = prompt.select("What colour would you like your #{kind} to be?", %w(Red Blue Yellow White Black Brown Green Pink Rainbow))
+colour = choice_of_colour.downcase
 
 puts "Yay! You have a #{colour} #{kind} now!"
-puts "Now it's time to choose an oufit for your #{kind}!/n"    
+puts "Now it's time to choose an oufit for your #{kind}!"
+
+puts "We have some special outfits in store. Would you like your #{kind} to wear one? Y or N?"
+answer = gets.chomp.downcase
+
+if answer == "y"
+choice_of_outfit = prompt.select("Great! Now choose a special outfit", %w( Doctor Firefigter Policeman Nurse Pilot Taxidriver Builder))
+ outfit = choice_of_outfit
+puts "Wow! Your #{kind} is a #{outfit}!!!"
+elsif answer == "n"
+choice_of_outfit = prompt.select("Ok! Let's choose what your #{kind} will wear then. What will you choose?", %w(Dress Overalls))
+outfit_colour = prompt.select("What colour #{choice_of_outfit}?", %w(Red Blue Yellow White Black Brown Green Pink )) 
+outfit = outfit_colour + " " + choice_of_outfit
+puts "Great! #{outfit} it is!"
+else
+puts "Invalid input"
+
+end
+
+
+
 
