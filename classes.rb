4617cb6c191f6b40ca19553ddb7d47088ef46ad3
MGorin84 <gorinamaria@gmail.com>
2019-11-14 16:07:08 +1100

    commit

--- classes.rb
+++ classes.rb
@@ -1,13 +1,23 @@
+
 class PlushToys
     attr_accessor :kind, :name, :colour, :accessories, :outfit
    
-   def initialize(kind, name, colour, accessories)
+   def initialize(kind, name, colour, outfit, accessories)
     @kind = kind
     @name = name
     @colour = colour
     @outfit = outfit
     @accessories = []
    end
+
+   def hello
+    puts "Hello! I am #{name} the #{colour.downcase} #{kind}. I am wearing a #{outfit} outfit  with my #{accessories[0]} and #{accessories[1]}"
+   end
+
+   def birth_certificate
+
+   end
+
 
 end
 
--- main.rb
+++ main.rb
@@ -19,22 +19,43 @@ colour = choice_of_colour.downcase
 puts "Yay! You have a #{colour} #{kind} now!"
 puts "Now it's time to choose an oufit for your #{kind}!"
 
+outfit = 0
+until outfit != 0
 puts "We have some special outfits in store. Would you like your #{kind} to wear one? Y or N?"
 answer = gets.chomp.downcase
-
-if answer == "y"
-choice_of_outfit = prompt.select("Great! Now choose a special outfit", %w( Doctor Firefigter Policeman Nurse Pilot Taxidriver Builder))
- outfit = choice_of_outfit
-puts "Wow! Your #{kind} is a #{outfit}!!!"
-elsif answer == "n"
-choice_of_outfit = prompt.select("Ok! Let's choose what your #{kind} will wear then. What will you choose?", %w(Dress Overalls))
-outfit_colour = prompt.select("What colour #{choice_of_outfit}?", %w(Red Blue Yellow White Black Brown Green Pink )) 
-outfit = outfit_colour + " " + choice_of_outfit
-puts "Great! #{outfit} it is!"
-else
-puts "Invalid input"
-
+    if answer == "y"
+    choice_of_outfit = prompt.select("Great! Now choose a special outfit", %w( Doctor Firefigter Policeman Nurse Pilot Taxidriver Builder))
+    outfit = choice_of_outfit
+    puts "Wow! Your #{kind} is a #{outfit}!!!"
+    elsif answer == "n"
+    choice_of_outfit = prompt.select("Ok! Let's choose what your #{kind} will wear then. What will you choose?", %w(Dress Overalls))
+    outfit_colour = prompt.select("What colour #{choice_of_outfit}?", %w(Red Blue Yellow White Black Brown Green Pink )) 
+    outfit = outfit_colour + " " + choice_of_outfit
+    puts "Great! #{outfit} it is!"
+    else
+    puts "Invalid input"
+    output = 0
+    end
 end
+
+choice_of_accessories = prompt.multi_select("Would you like to add some accessories? You can choose a few!!", %w(Backpack Umbrella Cap Headphones Sunglasses Glasses))
+
+accessories = choice_of_accessories
+puts accessories
+puts "Great! Almost there! Lets name your new friend!!!"
+name = gets.chomp
+puts "#{name} suit your #{kind} so much!!!"
+
+
+your_toy = PlushToys.new(kind, name, colour, outfit, accessories)
+
+your_toy.hello
+
+
+
+    
+
+
 
 
 
